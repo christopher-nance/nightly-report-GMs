9f3b0ae881fa2ace1ee2b5336b52baf51e73f5f7
WashU Data Server <admin@washucarwash.com>
2024-12-23 17:41:04 -0600

    Updated with new sonny site IDs from washmetrix --> NAPR/DP/CS/VP

--- report_generator.py
+++ report_generator.py
@@ -25,19 +25,16 @@ location_timezones = {
     '269529698b913dfb25f26ceace416fac': 'America/Chicago',  # Plainfield
     '718cfd435065af468cae6cfa655448dc': 'America/Chicago',  # Hub Office (Fed Hub | DRB [WSHUIL-HQ1])
     '441b92a084e0d23ac1f1efc1d4eb7367': 'America/Chicago',  # Query Server (ECOM | DRB [WSHUIL-HQ2])
-    '57a752239ecde15d76f4d0710a408e9f': 'America/Chicago',  # Villa Park (DRB | WSHUIL-002)
     '827ccde4158d5b6917661c3dd369341f': 'America/Chicago',  # Villa Park (VILLA)
     'da4f80608864892504784372113ba322': 'America/Chicago',  # Burbank
-    '46b724ec5bb89743f915bbe3ebf6b343': 'America/Chicago',  # Carol Stream
-    '88f23b2f258753f3a53cc223727a9189': 'America/Chicago',  # Des Plaines
+    '46b724ec5bb89743f915bbe3ebf6b343': 'America/Chicago',  # Carol Stream (CAROL)
+    '011f3e8d4c5c304c4a956de1eb89e75b': 'America/Chicago',  # Des Plaines (DESPLN)
     '88a2b3c8e7d1b7f1073d73de6b5a56f5': 'America/Chicago',  # Berwyn
     '39584e8cd2a8e19da2c9406faac47c2e': 'America/Chicago',  # Joliet
-    'd9febfa73cd20efaf95b28ff30d8e050': 'America/Chicago',  # Naperville
+    '220e853565de0c739bb3da29e512ed18': 'America/Chicago',  # Naperville (NAPER)
     '8a656bc1f58397ff7e026a3076411420': 'America/Chicago',  # Evergreen
-    '5203385e07d8589c1b5d07da8865e015': 'America/Los_Angeles',  # Fiesta
+    '5203385e07d8589c1b5d07da8865e015': 'America/Los_Angeles',  # Fiesta (FIESTA)
     'cf2257e6113a6298f68113a16929cad8': 'America/Los_Angeles',  # Centennial (CENT)
-    '1bd5671fa94b8023846b2935f590cc25': 'America/Los_Angeles',  # Centennial (SITE2)
-    '04814323fd60ab73484bf25cce5a2d68': 'America/Los_Angeles',  # Centennial (DRB | WSHUIL-101)
 }
 
 location_names = {
@@ -45,10 +42,10 @@ location_names = {
     'Villa Park':       '827ccde4158d5b6917661c3dd369341f',
     'Burbank':          'da4f80608864892504784372113ba322',
     'Carol Stream':     '46b724ec5bb89743f915bbe3ebf6b343',
-    'Des Plaines':      '88f23b2f258753f3a53cc223727a9189',
+    'Des Plaines':      '011f3e8d4c5c304c4a956de1eb89e75b',
     'Berwyn':           '88a2b3c8e7d1b7f1073d73de6b5a56f5',
     'Joliet':           '39584e8cd2a8e19da2c9406faac47c2e',
-    'Naperville':       'd9febfa73cd20efaf95b28ff30d8e050',
+    'Naperville':       '220e853565de0c739bb3da29e512ed18',
     'Evergreen Park':   '8a656bc1f58397ff7e026a3076411420',
     'Fiesta':           '5203385e07d8589c1b5d07da8865e015',
     'Centennial':       'cf2257e6113a6298f68113a16929cad8',
